65f6c35c1ab361b44ad21120b0543e91e47e86fe
Zarna M. Patel <zarna.patel@comp70.local>
2017-09-05 18:12:28 +0530

    Updated Animation

--- ParcticalTest/UniqueNumberVC.swift
+++ ParcticalTest/UniqueNumberVC.swift
@@ -10,27 +10,44 @@ import UIKit
 
 class UniqueNumberVC: UIViewController, UITableViewDataSource {
 
+    // Mark :- Variable declaration
     let reuseIdentifier = "Cell"
     var uNumber = [String]()
+    let defaults = UserDefaults.standard
+    var un = [String]()
     
+    //let defaults = UserDefaults.standard
+    
+    
+    // Mark :- Outlet
     @IBOutlet weak var tableOutlet: UITableView!
+    
+    // Mark :- View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "blur5.jpg")!)
         self.tableOutlet.backgroundColor = UIColor.clear
+        un = UserDefaults.standard.array(forKey: "uniqueNum") as! [String]
+        print(un)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-    
-    }
-    
+    // Mark :- TableView Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return uNumber.count
+        return un.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = ("Count of  is \(uNumber[indexPath.row])") as? String
+        cell.textLabel?.text = ("Count of  is \(String(describing: un[indexPath.row]))")
+        cell.layer.transform = CATransform3DMakeScale(0.1, 0.1, 1)
+        UIView.animate(withDuration: 1.3, animations: {
+            cell.layer.transform = CATransform3DMakeScale(1.05, 1.05, 1)
+        },completion: { finished in
+            UIView.animate(withDuration: 1.2, animations: {
+                cell.layer.transform = CATransform3DMakeScale(1, 1, 1)
+            })
+        })
+        cell.backgroundColor = UIColor.clear
         return cell
     }
 }
--- ParcticalTest/ViewController.swift
+++ ParcticalTest/ViewController.swift
@@ -10,35 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Mark :- Variable declaration
     var collectionView: UICollectionView?
-    var number: [Int] = []
+    var number: [String] = []
     var uniqueNumber = [String]()
+    let defaults = UserDefaults.standard
     
+    // Mark :- Outlets
     @IBOutlet weak var flowLayoutOutlet: UICollectionViewFlowLayout!
     @IBOutlet weak var collectionViewOutlet: UICollectionView!
     @IBOutlet weak var txtResult: UITextField!
+    @IBOutlet weak var findResult: UIButton!
+    @IBOutlet weak var countUniqueNumber: UIBarButtonItem!
+    @IBOutlet weak var findRandomValue: UIButton!
     
+    // Mark :- View lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         cellSize(view.frame.size, itemCount: 4)
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "blur5.jpg")!)
         self.collectionViewOutlet.backgroundColor = UIColor.clear
         self.txtResult.backgroundColor = UIColor.clear
+        findResult.isEnabled = false
+        countUniqueNumber.isEnabled = false
+        findRandomValue.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        animateButton()
     }
     
+    // Mark :- Button action
+    // Random Value button
     @IBAction func findRandomValue(_ sender: UIButton) {
-       
-        let lower : UInt32 = 0
-        let upper : UInt32 = 10
         for _ in 0 ..< 16 {
-            let randomNumber = arc4random_uniform(upper - lower) + lower
-            number.append(Int(randomNumber))
+            let randomNumber = arc4random() % 10
+            number.append(String(randomNumber))
         }
         collectionViewOutlet.reloadData()
+        findResult.isEnabled = true
+        countUniqueNumber.isEnabled = true
     }
-
-    
-    @IBAction func countUniqueNumber(_ sender: UIButton) {
+    // Unique number button
+    @IBAction func countUniqueNumber(_ sender: UIBarButtonItem) {
+        uniqueNumber.removeAll()
         for i in 0 ... 9 {
             var count = 0
             for j in 0 ..< number.count {
@@ -48,45 +60,73 @@ class ViewController: UIViewController {
                 }
             }
             if count != 0 {
-                print("count of \(i) is \(count)")
+                //print("count of \(i) is \(count)")
                 uniqueNumber.append("count of \(i) is \(count)")
             }
         }
-        let nxt = storyboard?.instantiateViewController(withIdentifier: "UniqueNumberVC") as! UniqueNumberVC
-        nxt.uNumber = uniqueNumber
-        self.navigationController?.pushViewController(nxt, animated: true)
-        //uniqueNumber.removeAll()
+        if uniqueNumber.count == 0 {
+            let alert = UIAlertController.init(title: "Number Not Found", message: "Press Random Value button", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(defaultAction)
+            present(alert, animated: true, completion: nil)
+        } else {
+            let nxt = storyboard?.instantiateViewController(withIdentifier: "UniqueNumberVC") as! UniqueNumberVC
+            defaults.removeObject(forKey: "uniqueNum")
+            defaults.set(uniqueNumber, forKey: "uniqueNum")
+            print(uniqueNumber)
+            //nxt.uNumber = uniqueNumber
+            self.navigationController?.pushViewController(nxt, animated: true)
+        }
     }
-    
+    // Sum button
     @IBAction func findResult(_ sender: UIButton) {
         var count :Int = 0
-        let input = Int(txtResult.text!)
-        var firstIndex:Int? = nil
-        for j in 0 ..< number.count {
-            let num = Int(number[j])
-            if input == num{
-                if firstIndex == nil {
-                    firstIndex = j
+        if txtResult.text == "" {
+            let alert = UIAlertController.init(title: "Enter Number", message: "Number Not Entered", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(defaultAction)
+            present(alert, animated: true, completion: nil)
+        } else {
+            let input = Int(txtResult.text!)
+            var firstIndex:Int? = nil
+            for j in 0 ..< number.count {
+                let num = Int(number[j])
+                if input == num {
+                    if firstIndex == nil {
+                        firstIndex = j
+                    }
+                    count = count + input!
                 }
-                count = count + input!
             }
-        }
-        for j in 0 ..< number.count {
-            if firstIndex == j{
-                number[j] = count
-            }else{
-                number[j] = Int("0")!
+            for j in 0 ..< number.count {
+                if firstIndex == j{
+                    number[j] = String(count)
+                } else {
+                    number[j] = String(" ")!
+                }
             }
+            collectionViewOutlet.reloadData()
         }
+    }
+    // Clear button
+    @IBAction func clearRandomValue(_ sender: UIBarButtonItem) {
+        number.removeAll()
+        txtResult.text = ""
+        findResult.isEnabled = false
+        countUniqueNumber.isEnabled = false
         collectionViewOutlet.reloadData()
     }
     
-    @IBAction func clearRandomValue(_ sender: UIBarButtonItem) {
-        number.removeAll()
-        collectionViewOutlet.reloadData()
+    func animateButton() {
+        findResult.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        UIView.animate(withDuration: 4.0,delay: 0,usingSpringWithDamping: 0.2,initialSpringVelocity: 6.0,options: .allowUserInteraction,animations: { [weak self] in
+            self?.findRandomValue.transform = .identity
+            self?.findResult.transform = .identity
+        },completion: nil)
     }
 }
 
+// Mark :- CollectionView Extention
 extension ViewController : UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -101,7 +141,7 @@ extension ViewController : UICollectionViewDataSource, UICollectionViewDelegateF
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
         cell.backgroundColor = UIColor.clear
         cell.layer.borderColor = UIColor.lightGray.cgColor
-        cell.layer.borderWidth = 1
+        cell.layer.borderWidth = 0
         cell.layer.cornerRadius = 0.5
         cell.textLabel.text = String(number[indexPath.row])
         return cell
@@ -111,9 +151,7 @@ extension ViewController : UICollectionViewDataSource, UICollectionViewDelegateF
         let cvFlowLayout = flowLayoutOutlet!
         let cvSize = size
         var cellSize = CGSize()
-        
         let minimumInteritemSpacingMultiplier:CGFloat = CGFloat(itemCount - 1)
-        
         cellSize.width = (cvSize.width - (cvFlowLayout.minimumInteritemSpacing * minimumInteritemSpacingMultiplier + cvFlowLayout.sectionInset.left + cvFlowLayout.sectionInset.right )) / CGFloat(itemCount)
         cellSize.height = cellSize.width
         cvFlowLayout.itemSize = cellSize
